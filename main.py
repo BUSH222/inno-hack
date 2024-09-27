fa4da3c1a86ff0823926d6ac06a28877ae685c8c
kust <vad.velikanov@yandex.ru>
2024-09-28 00:29:53 +0300

    endpoint fix

--- main.py
+++ main.py
@@ -8,6 +8,21 @@ app = Flask(__name__)
 def dashboard():
     return render_template("main.html")
 
+@app.route('/login')
+def login():
+    return "ok"
+
+@app.route('/signup')
+def signup():
+    return "ok"
+
+@app.route('/logout')
+def logout():
+    return "ok"
+
+@app.route('/account')
+def account():
+    return "ok"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
