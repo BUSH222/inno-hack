5bf4e9e4dd9aafda9860f177ea1feedbece2d06c
Ted <58144503+BUSH222@users.noreply.github.com>
2024-09-28 15:56:50 +0300

    flake8

--- main.py
+++ main.py
@@ -256,23 +256,7 @@ def init_repo():
     else:
         return None
 
-@app.route('/login')
-def login():
-    return "ok"
-
-@app.route('/signup')
-def signup():
-    return "ok"
-
-@app.route('/logout')
-def logout():
-    return "ok"
-
-@app.route('/account')
-def account():
-    return "ok"
 
 if __name__ == '__main__':
     preload_db()
     app.run(host='0.0.0.0', port=5000)
-
